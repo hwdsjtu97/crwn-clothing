b29add54da120ece235bbed66b181815aedfd583
Wendi Huang <hwdsjtu97@gmail.com>
2021-03-15 21:19:54 -0700

    updating app with shopPage, collection-item and collection-preveiw

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/shop/hats" component={HatsPage} />
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
